89bc125d0361466c8c4022aa17894adc2a4766be
Anish Kumar <anishk3843@gmail.com>
2026-02-14 09:28:42 +0530

    SEO: Standardized absolute URLs, added JSON-LD schema (SoftwareApp, Article, Breadcrumbs), updated sitemap, and added new comparison & security blog posts

--- PicToPDF/sw.js
+++ PicToPDF/sw.js
@@ -3,21 +3,22 @@
 
 const CACHE_NAME = 'pictopdf-website-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/privacy-policy.html',
-  '/css/styles.css',
-  '/js/script.js',
-  '/assets/app-icon-64.png',
-  '/assets/app-icon-192.png',
+  'https://avionti.in/PicToPDF/',
+  'https://avionti.in/PicToPDF/index.html',
+  'https://avionti.in/PicToPDF/privacy-policy.html',
+  'https://avionti.in/PicToPDF/blogs/',
+  'https://avionti.in/PicToPDF/css/styles.css',
+  'https://avionti.in/PicToPDF/js/script.js',
+  'https://avionti.in/PicToPDF/assets/app-icon-64.png',
+  'https://avionti.in/PicToPDF/assets/app-icon-192.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap'
 ];
 
 // Install event - cache resources
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
@@ -25,26 +26,26 @@ self.addEventListener('install', function(event) {
 });
 
 // Fetch event - serve from cache when offline
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         // Return cached version or fetch from network
         if (response) {
           return response;
         }
         return fetch(event.request);
       }
-    )
+      )
   );
 });
 
 // Activate event - clean up old caches
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheName !== CACHE_NAME) {
             console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
